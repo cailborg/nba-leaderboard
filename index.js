7c38484f640370461cf5df096591b6981281ea97
Cail Borg <cailborg@gmail.com>
2019-11-05 10:34:18 +1100

    Fuck you

--- index.js
+++ index.js
@@ -1,34 +1,35 @@
 const { teams } = require("./teams");
-var data = require("./data.json");
+// var data = require("./data.json");
+var fs = require("fs");
 
-console.log("hello");
+var json = JSON.parse(fs.readFileSync("./data.json", "utf8"));
+// console.log("json", json);
+// Store scores for each team here
+const scores = {};
 
-// function sort() {
-//   let value = data.filter(
-//     x =>
-//       (x.playerName === teams.moir.player1) |
-//       (x.playerName === teams.moir.player2) |
-//       (x.playerName === teams.moir.player3) |
-//       (x.playerName === teams.moir.player4) |
-//       (x.playerName === teams.moir.player5) |
-//       (x.playerName === teams.moir.player6) |
-//       (x.playerName === teams.moir.player7) |
-//       (x.playerName === teams.moir.player8) |
-//       (x.playerName === teams.moir.player9) |
-//       (x.playerName === teams.moir.player10)
-//   );
-//   console.log("value", value);
-// }
-// sort();
-
+// Loop over each team and check whether a player in data.json matches and assign to that team
 for (var team in teams) {
   let result = teams[team];
   let values = Object.values(result);
-  // console.log(team, Object.values(result));
-
+  let store = { teamName: team, players: {} };
+  var i = 0;
   for (const val of values) {
-    console.log("player", val);
-    let match = data.filter(x => x.playerName === val);
-    console.log("match", match);
+    i++;
+    let match = json.filter(x => x.playerName === val);
+
+    //Add up the points
+    let sum = match[0];
+    console.log("points", sum);
+
+    //Assign the matching result to the team object
+    Object.assign(store.players, { [i]: match });
   }
+  // console.log(store.teamName, store.players);
+
+  //Assign the team + players to the scores object
+  let test = { [store.teamName]: store.players };
+  Object.assign(scores, test);
 }
+
+// Do something with scores
+// console.log("scores", scores);
